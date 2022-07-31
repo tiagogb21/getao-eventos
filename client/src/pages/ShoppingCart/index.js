68599ae6e0ea968528f14fc2b5e4b61511126ef3
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-31 16:51:33 -0300

    criando shoppingcart

---
+++ client/src/pages/ShoppingCart/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function ShoppingCart() {
+  return (
+    <Container>
+      <h1>Carrinho de Compras</h1>
+
+    </Container>
+  );
+}
+
+export default ShoppingCart;
