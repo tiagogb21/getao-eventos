5edd568694d8f765788c3d57b792d963825cbb39
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-31 11:06:08 -0300

    criando database gestao_eventos

---
+++ server/database.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE IF NOT EXISTS gestao_eventos;
+
+USE gestao_eventos;
+
+CREATE TABLE users
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	full_name VARCHAR(30) NOT NULL,
+	user_email VARCHAR(30) NOT NULL,
+  user_password VARCHAR(30) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+INSERT INTO users (full_name, user_email, user_password);
+
+VALUES ('admin', 'admin@admin.com', 'admin');
+
+CREATE TABLE eventos
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	cidade VARCHAR(30) NOT NULL,
+	estado VARCHAR(30) NOT NULL,
+  tipo VARCHAR(30) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+INSERT INTO eventos (cidade, estado, tipo);
+
+VALUES ('sao paulo', 'sao paulo', 'musica');
+
+CREATE TABLE pagamentos
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	qtd_ingresso INT NOT NULL,
+  birthday DATE,
+	PRIMARY KEY(id)
+);
+
+INSERT INTO pagamentos (qtd_ingresso);
+
+VALUES (0);
