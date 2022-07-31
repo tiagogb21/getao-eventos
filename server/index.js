736d5c78ce66210558b401142c2f6d7a1e17c031
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-31 10:51:08 -0300

    configurando servidor

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require("cors")
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log("Rodando na porta 3001.");
+});
