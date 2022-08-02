e9f59dea7b376df4fd62636c29f6150a455296f9
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-02 19:12:30 -0300

    <backend-user>criando as funções getbyid, update e delete

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -9,6 +9,17 @@ const getAll = async (req, res) => {
   }
 };
 
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = UserView.getById(id);
+    if (!user) return res.status(401).json({ message: "User not found!" });
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 const createUser = async (req, res) => {
   try {
     const { full_name, user_email, user_password } = req.body;
@@ -18,6 +29,28 @@ const createUser = async (req, res) => {
       user_password
     );
     return res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await UserView.getById(id);
+    if (!user) return res.status(401).json({ message: "User not found!" });
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await UserView.deleteUser(id);
+    if (!user) return res.status(401).json({ message: "User not found!" });
+    return res.status(200).json(user);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
