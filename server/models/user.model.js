43beac4b736a3bff49e1660bee3610f4f775e0a5
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-02 19:56:10 -0300

    <backend> <user> criando getbyid, update e delete

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -6,6 +6,12 @@ const getAll = async () => {
   return users;
 };
 
+const getById = async (id) => {
+  const query = "SELECT * FROM GestaoEventos.users WHERE id = ?";
+  const [users] = await connection.execute(query, id);
+  return users;
+};
+
 const createUser = async (full_name, user_email, user_password) => {
   const query =
     "INSERT INTO GestaoEventos.users (full_name, user_email, user_password) VALUES (?, ?, ?)";
@@ -22,4 +28,16 @@ const createUser = async (full_name, user_email, user_password) => {
   };
 };
 
-module.exports = { getAll, createUser };
+const updateUser = async (id, full_name, user_email, user_password) => {
+  const query =
+    "UPDATE GestaoEventos.users SET full_name = ?, user_email = ?, user_password = ? WHERE id = ?";
+  await connection.execute(query, [full_name, user_email, user_password]);
+  return {
+    id,
+    full_name,
+    user_email,
+    user_password,
+  };
+};
+
+module.exports = { getAll, createUser, updateUser };
--- server/views/user.view.js
+++ server/views/user.view.js
@@ -1,13 +1,22 @@
-const UserModel = require('../models/user.model');
+const UserModel = require("../models/user.model");
 
 const getAll = async () => {
   const users = await UserModel.getAll();
   return users;
-}
+};
+
+const getById = async () => {
+  const user = await UserModel.getById();
+  return user;
+};
 
 const createUser = async (full_name, user_email, user_password) => {
-  const users = await UserModel.createUser(full_name, user_email, user_password);
+  const users = await UserModel.createUser(
+    full_name,
+    user_email,
+    user_password
+  );
   return users;
-}
+};
 
-module.exports = { getAll, createUser }
+module.exports = { getAll, createUser, getById, updateUser, deleteUser };
