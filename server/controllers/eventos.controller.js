08cf88d7a7110041f7abe0f30e776817d11e7c6c
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-03 12:18:37 -0300

    <backend> criando a controller para eventos

--- server/controllers/eventos.controller.js
+++ server/controllers/eventos.controller.js
@@ -9,10 +9,22 @@ const getAll = async (req, res) => {
   }
 };
 
-const createEventos = async (req, res) => {
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const evento = await EventosView.getById(id);
+    if (evento.length === 0)
+      return res.status(404).json({ message: "Event not found!" });
+    return res.status(200).json(evento);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const createEvento = async (req, res) => {
   try {
     const { cidade, estado, evento, tipo, turno, preco } = req.body;
-    const eventos = await EventosView.createEventos(
+    const eventos = await EventosView.createEvento(
       cidade,
       estado,
       evento,
@@ -26,4 +38,36 @@ const createEventos = async (req, res) => {
   }
 };
 
-module.exports = { getAll, createEventos };
+const updateEvento = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { cidade, estado, evento, tipo, turno, preco } = req.body;
+    const evento = await EventosView.updateEvento(
+      cidade,
+      estado,
+      evento,
+      tipo,
+      turno,
+      preco
+    );
+    if (evento.length === 0)
+      return res.status(404).json({ message: "Event not found!" });
+    return res.status(200).json({ message: "Updated event!" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteEvento = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const evento = await EventosView.deleteEvento(id);
+    if (evento.length === 0)
+      return res.status(404).json({ message: "Event not found!" });
+    return res.status(200).json({ message: "Deleted event!" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getAll, getById, createEvento, updateEvento, deleteEvento };
--- server/route/eventos.route.js
+++ server/route/eventos.route.js
@@ -5,6 +5,9 @@ const EventosController = require("../controllers/eventos.controller");
 const router = express.Router();
 
 router.get("/eventos", EventosController.getAll);
+router.get("/eventos/:id", EventosController.getById);
 router.post("/eventos", EventosController.createEventos);
+router.put("/eventos/:id", EventosController.updateEvento);
+router.delete("/eventos/:id", EventosController.deleteEvento);
 
 module.exports = router;
