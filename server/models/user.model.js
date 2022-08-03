33d291d19c11ba7196c875c002003869983ad2e0
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-08-03 17:19:41 -0300

    <frontend> estilizando eventos

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,11 +1,10 @@
-const connection = require("./connection");
+const connection = require('./connection');
 
 const getAll = async () => {
-  const query = "SELECT * FROM GestaoEventos.users";
+  const query = 'SELECT * FROM GestaoEventos.users';
   const [users] = await connection.execute(query);
   const user = users.reduce((acc, curr) => {
     const { user_password, ...userWithoutPass } = curr;
-    console.log(userWithoutPass);
     acc.push(userWithoutPass);
     return acc;
   }, []);
@@ -13,7 +12,7 @@ const getAll = async () => {
 };
 
 const getById = async (id) => {
-  const query = "SELECT * FROM GestaoEventos.users WHERE id = ?";
+  const query = 'SELECT * FROM GestaoEventos.users WHERE id = ?';
   const [users] = await connection.execute(query, [id]);
   return users;
 };
@@ -23,32 +22,32 @@ const createUser = async (full_name, user_email, user_password) => {
   const userExists = users.some((user) => user.user_email === user_email);
   if (userExists) return;
   const query =
-    "INSERT INTO GestaoEventos.users (full_name, user_email, user_password) VALUES (?, ?, ?)";
+    'INSERT INTO GestaoEventos.users (full_name, user_email, user_password) VALUES (?, ?, ?)';
   const [{ insertId: id }] = await connection.execute(query, [
     full_name,
     user_email,
-    user_password,
+    user_password
   ]);
   return {
     id,
     full_name,
-    user_email,
+    user_email
   };
 };
 
 const updateUser = async (id, full_name, user_email, user_password) => {
   const query =
-    "UPDATE GestaoEventos.users SET full_name = ?, user_email = ?, user_password = ? WHERE id = ?";
+    'UPDATE GestaoEventos.users SET full_name = ?, user_email = ?, user_password = ? WHERE id = ?';
   await connection.execute(query, [full_name, user_email, user_password, id]);
   return {
     id,
     full_name,
-    user_email,
+    user_email
   };
 };
 
 const deleteUser = async (id) => {
-  const query = "DELETE FROM GestaoEventos.users WHERE id = ?";
+  const query = 'DELETE FROM GestaoEventos.users WHERE id = ?';
   const delUser = await connection.execute(query, [id]);
   return delUser;
 };
